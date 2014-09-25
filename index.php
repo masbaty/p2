679a6306c95af231367503133013e4c44fa9e6ea
masbaty <masbaty@gmail.com>
2014-09-25 19:18:12 -0400

    Started working on index.php and created logic.php and style.css

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	error_reporting(-1); #Report all PHP errors
+	ini_set('display_errors', 1);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Project 2</title>
+	<link rel='stylesheet' href='style.css' type='text/css'>
+	<?php require("logic.php");?>
+</head>
+
+<body>
+
+	
+
+</body>
+</html>
