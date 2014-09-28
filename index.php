91ad24094cea8707a2322be5303f9812ba10ea00
masbaty <masbaty@gmail.com>
2014-09-28 12:04:37 -0400

    Modified index.php and logic.php

--- index.php
+++ index.php
@@ -7,14 +7,32 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Project 2</title>
+	<title>Project 2: Password Generator</title>
 	<link rel='stylesheet' href='style.css' type='text/css'>
 	<?php require("logic.php");?>
 </head>
 
 <body>
+	<h1>xkcd Password Generator</h1>
+	<h2>Your Password</h2>
+	<p>Your password is: </p>
 
-	
+	<form method="POST" action="index.php">
+
+		<label name="count">How many words?</label>
+			<input type="text" id="count" name="count" /><br />
+
+		<label name="uppercase">Uppercase at beginning of each word?</label>
+			<input type="checkbox" name="uppercase" value="uppercase" /><br />
+
+		<label name="symbol">Use any symbols?</label>
+			<input type="checkbox" name="symbol" value="symbol" /><br />
+
+		<label name="number">Include a number?</label>
+			<input type="checkbox" name="number" value="number" /><br />
+
+		<input type="submit" name="submit" value="submit" />
+	</form>
 
 </body>
 </html>
---
+++ logic.php
@@ -0,0 +1,28 @@
+<?php
+
+	if (isset($_POST["count"])) {
+		echo $_POST["count"];
+	}
+
+	if (isset($_POST["uppercase"])) {
+		echo $_POST["uppercase"];
+	}
+
+	if (isset($_POST["symbol"])) {
+		echo $_POST["symbol"];
+	}
+
+	if (isset($_POST["number"])) {
+		echo $_POST["number"];
+	}
+
+	# Pull data from form
+
+
+	# Run logic on form input
+
+
+	# Update variables to send back
+
+
+	# Cleanup with helper functions
